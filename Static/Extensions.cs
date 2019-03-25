96a3e3e44f3d6328311d7f570329330a0c446b7b
KAHA <wannasaynone@gmail.com>
2019-03-25 21:40:10 +0800

    add Extensions, add extension method to calculate direction

---
+++ Static/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityEngine
+{
+    public static class Extensions
+    {
+        public static Vector3 Direction(this Transform transform, Vector3 target)
+        {
+            return (target - transform.position).normalized;
+        }
+    }
+}
