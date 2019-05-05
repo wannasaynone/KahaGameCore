e29ef53ae1eea96c5dc6e6f7d4b0f20931463916
KAHA <wannasaynone@gmail.com>
2019-05-05 18:02:44 +0800

    0.9.46

--- Editor/KahaGameCoreUpdater.cs
+++ Editor/KahaGameCoreUpdater.cs
@@ -26,6 +26,7 @@ namespace KahaGameCore.Editor
 
         private static bool m_isChecking = false;
         private static string m_packageFileName = "KahaGameCore-{0}.unitypackage";
+        private static string m_versionText = "";
         private static string m_path = "";
 
         [MenuItem("Tools/Update KahaGameCore")]
@@ -50,32 +51,35 @@ namespace KahaGameCore.Editor
         private static void OnGotPackageJsonString(object sender, DownloadStringCompletedEventArgs e)
         {
             string _localVersionTextFilePath = Path.Combine(Application.persistentDataPath, "version.txt");
+            m_versionText = JsonReader.Deserialize<PackageData>(e.Result).version;
 
-            string _gitVersioString = JsonReader.Deserialize<PackageData>(e.Result).version;
             string _localVersionString = "";
-
-            if(!File.Exists(_localVersionTextFilePath))
+            if(File.Exists(_localVersionTextFilePath))
             {
-                using (StreamWriter _streamWriter = new StreamWriter(_localVersionTextFilePath))
-                {
-                    _streamWriter.Write(_gitVersioString);
-                }
+                _localVersionString = File.ReadAllText(_localVersionTextFilePath);
+            }
+            else
+            {
+                Debug.Log("Can't find version text file, will force update for this time");
             }
 
-            _localVersionString = File.ReadAllText(_localVersionTextFilePath);
-
-            if (_localVersionString == _gitVersioString)
+            if (_localVersionString == m_versionText)
             {
-                Debug.LogFormat("Is Newest Version {0}", _gitVersioString);
+                Debug.LogFormat("Is Newest Version {0}", m_versionText);
                 m_isChecking = false;
                 return;
             }
             else
             {
-                Debug.LogFormat("Has Newer Version {0}(current={1}), Start Updating Package...", _gitVersioString, _localVersionString);
+                Debug.LogFormat("Start updating package, version={0}(current={1})", m_versionText, _localVersionString);
+                if(File.Exists(Path.Combine(Application.persistentDataPath, string.Format(m_packageFileName, _localVersionString))))
+                {
+                    File.Delete(Path.Combine(Application.persistentDataPath, string.Format(m_packageFileName, _localVersionString)));
+                    Debug.Log("Old package deleted:" + Path.Combine(Application.persistentDataPath, string.Format(m_packageFileName, _localVersionString)));
+                }
             }
 
-            m_path = Path.Combine(Application.persistentDataPath, string.Format(m_packageFileName, _gitVersioString));
+            m_path = Path.Combine(Application.persistentDataPath, string.Format(m_packageFileName, m_versionText));
 
             if (File.Exists(m_path))
             {
@@ -85,11 +89,11 @@ namespace KahaGameCore.Editor
 
             using (WebClient _client = new WebClient())
             {
-                Debug.Log("Start Updating Package...");
+                Debug.Log("Start Downloading new package...");
                 m_isChecking = true;
                 _client.DownloadProgressChanged += OnDownloadProgressChanged;
                 _client.DownloadFileCompleted += OnDownloadComplete;
-                _client.DownloadFileAsync(new Uri(string.Format(m_packageUri, _gitVersioString)), m_path);
+                _client.DownloadFileAsync(new Uri(string.Format(m_packageUri, m_versionText)), m_path);
             }
         }
 
@@ -101,7 +105,11 @@ namespace KahaGameCore.Editor
         private static void OnDownloadComplete(object sender, AsyncCompletedEventArgs e)
         {
             m_isChecking = false;
-            Debug.Log("Download Complete:" + m_path);
+            Debug.Log("Download Complete:" + m_path + ", version=" + m_versionText);
+            using (StreamWriter _streamWriter = new StreamWriter(Path.Combine(Application.persistentDataPath, "version.txt")))
+            {
+                _streamWriter.Write(m_versionText);
+            }
             AssetDatabase.ImportPackage(m_path, true);
         }
     }
