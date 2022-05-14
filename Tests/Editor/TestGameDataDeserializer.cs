02bc95352d7b07849ef878fbdf60ddf1fba683f4
KAHA <wannasaynone@hotmail.com>
2022-05-14 23:34:21 +0800

    add test

---
+++ Tests/Editor/TestGameDataDeserializer.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+
+namespace KahaGameCore.Tests
+{
+    public class TestGameDataDeserializer
+    {
+        private readonly string testJson = "[\n" +
+                                                 "{ " +
+                                                   "  \"ID\" : 1,\n" +
+                                                   "  \"TestStringField\" : \"Test String\",\n" +
+                                                   "  \"TestFloatField\" : 3.14\n" +
+                                                "},\n" +
+                                                "{ " +
+                                                 "  \"ID\" : 2,\n" +
+                                                   "  \"TestStringField\" : \"Test String 2\",\n" +
+                                                "  \"TestFloatField\" : 6.28\n" +
+                                                "}\n" +
+                                           "]";
+
+        private class TestData
+        {
+            public int ID { get; private set; }
+            public string TestStringField { get; private set; }
+            public float TestFloatField { get; private set; }
+
+            public TestData() { }
+        }
+
+        [Test]
+        public void GameDataDeserializerTest()
+        {
+            Common.GameStaticDataDeserializer deserializer = new Common.GameStaticDataDeserializer();
+            TestData[] datas = deserializer.Read<TestData[]>(testJson);
+
+            Assert.IsNotNull(datas);
+            Assert.AreEqual(2, datas.Length);
+            Assert.AreEqual(2, datas[1].ID);
+            Assert.AreEqual("Test String", datas[0].TestStringField);
+        }
+    }
+}
--- Tests/Editor/TestGameDataSerializer.cs
+++ Tests/Editor/TestGameDataSerializer.cs
@@ -2,40 +2,21 @@ using NUnit.Framework;
 
 namespace KahaGameCore.Tests
 {
-    public class TestGameDataDeserializer
+    public class TestGameDataSerializer 
     {
-        private readonly string testJson = "[\n" +
-                                                 "{ " +
-                                                   "  \"ID\" : 1,\n" +
-                                                   "  \"TestStringField\" : \"Test String\",\n" +
-                                                   "  \"TestFloatField\" : 3.14\n" +
-                                                "},\n" +
-                                                "{ " +
-                                                 "  \"ID\" : 2,\n" +
-                                                   "  \"TestStringField\" : \"Test String 2\",\n" +
-                                                "  \"TestFloatField\" : 6.28\n" +
-                                                "}\n" +
-                                           "]";
-
-        private class TestData
+        public class TestData
         {
-            public int ID { get; private set; }
-            public string TestStringField { get; private set; }
-            public float TestFloatField { get; private set; }
-
-            public TestData() { }
+            public int SomeValue;
         }
 
         [Test]
-        public void GameDataDeserializerTest()
+        public void GameDataSerializerTest()
         {
-            Common.GameStaticDataDeserializer deserializer = new Common.GameStaticDataDeserializer();
-            TestData[] datas = deserializer.Read<TestData[]>(testJson);
+            Common.GameStaticDataSerializer serializer = new Common.GameStaticDataSerializer();
 
-            Assert.IsNotNull(datas);
-            Assert.AreEqual(2, datas.Length);
-            Assert.AreEqual(2, datas[1].ID);
-            Assert.AreEqual("Test String", datas[0].TestStringField);
+            string json = serializer.Write(new TestData { SomeValue = 100 });
+
+            Assert.AreEqual("{\"SomeValue\":100}", json.Trim());
         }
     }
 }
